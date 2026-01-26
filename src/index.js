827b33d42c6f1ee0ee672ea94aaab550fe8a2bf0
Muhammadali <rahimov.dev1@gmail.com>
2026-01-26 10:06:49 +0500

    fix error

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Sign_in from './Pages/Sign_in/Sign_in';
-import Sign_up from './Pages/Sign_up/Sign_up';
+
+import SignIn from './Pages/Sign_in/SignIn';
+import SignUp from './Pages/Sign_up/SignUp';
 import ReadingForm from './Pages/ReadingForm/ReadingForm';
 import Reading from './Pages/Reading/Banner';
 import Navbar from './components/Navbar/Navbar';
 import Account from './Pages/Account/Account';
 import Select from './Pages/Select/Select';
-import Teacher_in from './Pages/Teacher_up/Teacher_in';
-import Select_in from './Pages/Select_in/Select_in';
+import TeacherIn from './Pages/Teacher_up/TeacherIn';
+import SelectIn from './Pages/Select_in/SelectIn';
 import Teacher from './Pages/Teacher/Teacher';
 import TeachAcc from './Pages/Teacher_acc/TeachAcc';
 import Solving from './Pages/Solving/Solving';
@@ -19,12 +20,12 @@ import Students from './Pages/Students/Students';
 import Listening from './Pages/Listening/Listening';
 import SelectT from './Pages/SelectTest/SelectT';
 import ListeningForm from './Pages/ListningForm/ListeningForm';
-import SolvingL from "./Pages/listeningSolving/Solving"
+import SolvingL from "./Pages/listeningSolving/Solving";
 import Writing from './Pages/Writing/Writing';
 import Home from './Pages/Home/Home';
 import WritingForm from './Pages/WritingForm/Writing';
 import WritingPage from './Pages/WritingPage/Index';
-import WResault from './Pages/Writing_resault/wResault';
+import WResult from './Pages/Writing_resault/WResult';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -34,15 +35,15 @@ root.render(
       <App />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/sign_in' element={<Sign_in />} />
-        <Route path='/sign_up' element={<Sign_up />} />
+        <Route path='/sign_in' element={<SignIn />} />
+        <Route path='/sign_up' element={<SignUp />} />
         <Route path='/reading/:testId' element={<ReadingForm />} />
         <Route path='/read' element={<Reading />} />
         <Route path='/account' element={<Account />} />
         <Route path='/select' element={<Select />} />
         <Route path='/teacher' element={<Teacher />} />
-        <Route path='/select_in' element={<Select_in />} />
-        <Route path='/teacher_in' element={<Teacher_in />} />
+        <Route path='/select_in' element={<SelectIn />} />
+        <Route path='/teacher_in' element={<TeacherIn />} />
         <Route path='/teachacc' element={<TeachAcc />} />
         <Route path='/solving' element={<Solving />} />
         <Route path='/students' element={<Students />} />
@@ -53,10 +54,8 @@ root.render(
         <Route path='/writing' element={<Writing />} />
         <Route path='/writingform' element={<WritingForm />} />
         <Route path='/writingpage/:id' element={<WritingPage />} />
-        <Route path='/wresault' element={<WResault />} />
+        <Route path='/wresault' element={<WResult />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
-
-
